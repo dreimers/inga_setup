b15ec7ad5f6e41264da2a03ec703645638e68384
Dennis Reimers <d.reimers@tu-bs.de>
2015-01-08 09:03:56 +0100

    Makes use of bootloader check to flash only if needed and make more attempts in case of problems

--- inga_setup_tools/run_initial_setup.sh
+++ inga_setup_tools/run_initial_setup.sh
@@ -12,8 +12,10 @@ SERIAL=$1
 JOBNAME="node-"$SERIAL
 LOGFILE="logs/initial_setup-$(timestamp).log"
 INGA_DEVICE_PATH="/dev/inga"
-DEVICES=()
+NODES=()
 
+# Attempts to flash to bootloader
+MAX_BOOTLOADER_ATTEMPS=3
 export LOGFILE
 
 
@@ -21,38 +23,69 @@ print_info "Logging to $LOGFILE"
 # print_info redirects output to file if and only if $LOGFILE exists so create it here
 touch $LOGFILE
 print_info "### Running initial setup ###"
-print_info "Searching for nodes..."
+print_info "# Step 1 - Searching for nodes..."
 
 if [ -a $INGA_DEVICE_PATH ];
 then
-  for DEVICE in $(find $INGA_DEVICE_PATH -name "node-*")
+  for NODE in $(find $INGA_DEVICE_PATH -name "node-*")
   do
-    print_info "found node at $DEVICE"
-    DEVICES+=("$DEVICE")
+    print_info "found node at $NODE"
+    NODES+=("$NODE")
   done
 
-  NUMBER_OF_DEVICES_FOUND="${#DEVICES[@]}"
+  NUMBER_OF_NODES_FOUND="${#NODES[@]}"
 else
   print_info "Path $INGA_DEVICE_PATH does not exist. Possible reasons:"
-  print_info "no device connected, udev-rule not working"
-  print_info "or device(s) are missing FTDI EEPROM configuration"
-  NUMBER_OF_DEVICES_FOUND=0
+  print_info "no node connected, udev-rule not working"
+  print_info "or node(s) are missing FTDI EEPROM configuration"
+  NUMBER_OF_NODES_FOUND=0
 fi
 
-print_info "$NUMBER_OF_DEVICES_FOUND nodes found"
+print_info "$NUMBER_OF_NODES_FOUND nodes found"
 
-if [ $NUMBER_OF_DEVICES_FOUND -eq "0" ];
+if [ $NUMBER_OF_NODES_FOUND -eq "0" ];
 then
-  print_info "No devices found aborting"
+  print_info "No nodes found aborting"
   exit
 fi
 
-print_info "Flashing bootloader through FTDI Bitbang..."
+print_info "# Step 2 - Preparing nodes with bootloader"
 
-for DEVICE in ${DEVICES[@]};
+# node counter
+let DEV_NO=1
+
+for NODE in ${NODES[@]};
 do
-  print_info "Trying node $DEVICE"
-  ./flash_bootloader $DEVICE
+  print_info "Trying node $NODE ($((DEV_NO++))/$NUMBER_OF_NODES_FOUND):"
+
+  for TRYNR in `seq 1 $MAX_BOOTLOADER_ATTEMPS`;
+  do
+    ./check_bootloader $NODE
+    NOT_HAS_BOOTLOADER=$?
+
+    if [ $NOT_HAS_BOOTLOADER -eq 0 ]; then
+      print_info "Found bootloader on $NODE"". Excellent."
+      NODES_W_BOOTLOADER+=("$NODE")
+      break
+    else
+      print_info "Bootloader is missing"
+      print_info "Flashing bootloader through FTDI Bitbang... TryNo ($TRYNR/$MAX_BOOTLOADER_ATTEMPS)"
+      ./flash_bootloader $NODE
+      # TODO workaround for missing devices after runnnig avrdude
+      sudo modprobe -r ftdi_sio && sudo modprobe ftdi_sio
+      #
+      if [ $TRYNR -eq $MAX_BOOTLOADER_ATTEMPS ];
+      then
+        print_info "Node $NODE is still missing bootloader. Skipping node for now."
+      fi
+    fi
+  done
+done
+
+
+for NODE in ${NODES_W_BOOTLOADER[@]};
+do
+  print_info "Trying node $NODE"
 done
 
 
